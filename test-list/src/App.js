42ca011ff674b97f4eedb34cf00fc327321aac76
Hailagaz <omentielvo.lumenn@gmail.com>
2023-08-02 21:19:28 +0300

    step1

--- test-list/src/App.js
+++ test-list/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ItemList from './components/ItemList';
 
 function App() {
 	return (
 		<div className="App">
-			<p>Initial</p>
+			<ItemList />
 		</div>
 	);
 }
